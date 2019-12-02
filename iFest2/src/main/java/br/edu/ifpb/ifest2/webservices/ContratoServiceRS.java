b77ad40991f45eb88587878654180329b90c9e9a
Danilo Rodrigues dos Santos <danilo.rodrigues@academico.ifpb.edu.br>
2019-12-02 08:15:16 -0200

    Contrato ServiceRS

--- iFest2/src/main/java/br/edu/ifpb/ifest2/webservices/ContratoServiceRS.java
+++ iFest2/src/main/java/br/edu/ifpb/ifest2/webservices/ContratoServiceRS.java
@@ -1,10 +1,12 @@
 package br.edu.ifpb.ifest2.webservices;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -25,7 +27,7 @@ public class ContratoServiceRS {
 		@Path("/nome/{id}")
 		@Produces("text/html")
 		public String getNome(@PathParam("id") Long id) {
-			return "<h1>"+ Contrato.getByID(id).getNome()+"</h1>";
+			return "<h1>"+ contratoService.getByID(id).getDataInicio()+"</h1>";
 		}
 
 	// http://localhost:8080/ifest2/rest/user/list
@@ -36,7 +38,7 @@ public class ContratoServiceRS {
 			String retorno = "<ul>";
 			List<Contrato> contrato = contratoService.getAll();
 			for(Contrato c: contrato) {
-				retorno += "<li>"+c.getNome()+"</li>";
+				retorno += "<li>"+c.getId()+"</li>";
 			}
 			return retorno + "</ul>";
 		}
@@ -46,7 +48,14 @@ public class ContratoServiceRS {
 		@Path("/add/{contrato}")
 		public String add(@PathParam("contrato") PathSegment atributos) {
 			MultivaluedMap<String, String> matrixParameters = atributos.getMatrixParameters();
+			SimpleDateFormat s = new SimpleDateFormat("dd/MM/aaaa");
+			
 			String dataInicio = matrixParameters.getFirst("inicio");
+			try {
+				Date data = s.parse(dataInicio);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
 			String dataTermino = matrixParameters.getFirst("termino");
 			String id = matrixParameters.getFirst("id");
 			String prorrogacaoContrato = matrixParameters.getFirst("prorrogacao");
